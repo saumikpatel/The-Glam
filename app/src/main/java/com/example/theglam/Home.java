1c5ab1a7c5ebac50ab58368b8664b2d0615ccdde
smit <smitpanchal14@gmail.com>
2020-08-04 00:30:43 -0400

    prodcut list added  for all category

--- app/src/main/java/com/example/theglam/Home.java
+++ app/src/main/java/com/example/theglam/Home.java
@@ -5,6 +5,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.theglam.adapter.ProductAdapter;
 import com.example.theglam.adapter.ProductCategoryAdapter;
@@ -18,12 +20,20 @@ public class Home extends AppCompatActivity {
     ProductCategoryAdapter productCategoryAdapter;
     RecyclerView productCatRecycler, prodItemRecycler;
     ProductAdapter productAdapter;
+    TextView hair, body, skin,face;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        hair=(TextView) findViewById(R.id.hair);
+        body=(TextView)findViewById(R.id.body);
+        skin=(TextView)findViewById(R.id.skin);
+        face=(TextView)findViewById(R.id.face);
+
+
         List<ProductCategory> productCategoryList = new ArrayList<>();
+        final List<Products> productsList = new ArrayList<>();
         productCategoryList.add(new ProductCategory(1, "Trending"));
         productCategoryList.add(new ProductCategory(2, "Most Popular"));
         productCategoryList.add(new ProductCategory(3, "All Body Products"));
@@ -33,8 +43,47 @@ public class Home extends AppCompatActivity {
         productCategoryList.add(new ProductCategory(7, "Fragrance"));
 
         setProductRecycler(productCategoryList);
+        bodyproduct(productsList);
 
-        List<Products> productsList = new ArrayList<>();
+
+        hair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                removeitem(productsList);
+                hairproduct(productsList);
+            }
+        });
+        body.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                removeitem(productsList);
+                bodyproduct(productsList);
+            }
+        });
+        skin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                removeitem(productsList);
+                skinproduct(productsList);
+            }
+        });
+        face.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                removeitem(productsList);
+                faceprodcut(productsList);
+            }
+        });
+
+    }
+
+
+
+
+
+
+    private void bodyproduct(List<Products> productsList){
+
         productsList.add(new Products(1, "Japanese Cherry Blossom", "250 ml", "$ 17.00", R.drawable.prod2));
         productsList.add(new Products(2, "African Mango Shower Gel", "350 ml", "$ 25.00", R.drawable.prod1));
         productsList.add(new Products(1, "Japanese Cherry Blossom", "250 ml", "$ 17.00", R.drawable.prod2));
@@ -44,6 +93,52 @@ public class Home extends AppCompatActivity {
 
         setProdItemRecycler(productsList);
     }
+
+    private void hairproduct(List<Products> productsList){
+
+        productsList.add(new Products(1, "Hair product 1", "250 ml", "$ 17.00", R.drawable.prod2));
+        productsList.add(new Products(2, "Hair product 1", "350 ml", "$ 25.00", R.drawable.prod1));
+        productsList.add(new Products(1, "Hair product 1", "250 ml", "$ 17.00", R.drawable.prod2));
+        productsList.add(new Products(2, "Hair product 1", "350 ml", "$ 25.00", R.drawable.prod1));
+        productsList.add(new Products(1, "Hair product 1", "250 ml", "$ 17.00", R.drawable.prod2));
+        productsList.add(new Products(2, "Hair product 1", "350 ml", "$ 25.00", R.drawable.prod1));
+
+        setProdItemRecycler(productsList);
+    }
+    private void skinproduct(List<Products> productsList){
+
+        productsList.add(new Products(1, "Skin product 1", "250 ml", "$ 17.00", R.drawable.prod2));
+        productsList.add(new Products(2, "Skin product 1", "350 ml", "$ 25.00", R.drawable.prod1));
+        productsList.add(new Products(1, "Skin product 1", "250 ml", "$ 17.00", R.drawable.prod2));
+        productsList.add(new Products(2, "Skin product 1", "350 ml", "$ 25.00", R.drawable.prod1));
+        productsList.add(new Products(1, "Skin product 1", "250 ml", "$ 17.00", R.drawable.prod2));
+        productsList.add(new Products(2, "Skin product 1", "350 ml", "$ 25.00", R.drawable.prod1));
+
+        setProdItemRecycler(productsList);
+    }
+    private void faceprodcut(List<Products> productsList){
+
+        productsList.add(new Products(1, "Face product 1", "250 ml", "$ 17.00", R.drawable.prod2));
+        productsList.add(new Products(2, "Face product 1", "350 ml", "$ 25.00", R.drawable.prod1));
+        productsList.add(new Products(1, "Face product 1", "250 ml", "$ 17.00", R.drawable.prod2));
+        productsList.add(new Products(2, "Face product 1", "350 ml", "$ 25.00", R.drawable.prod1));
+        productsList.add(new Products(1, "Face product 1", "250 ml", "$ 17.00", R.drawable.prod2));
+        productsList.add(new Products(2, "Face product 1", "350 ml", "$ 25.00", R.drawable.prod1));
+
+        setProdItemRecycler(productsList);
+    }
+
+    private void removeitem(List<Products> productsList)
+    {
+        int size = productsList.size();
+        if (size > 0) {
+            for (int i = 0; i < size; i++) {
+                productsList.remove(0);
+            }
+        }
+    }
+
+
     private void setProductRecycler(List<ProductCategory> productCategoryList){
 
         productCatRecycler = findViewById(R.id.cat_recycler);
