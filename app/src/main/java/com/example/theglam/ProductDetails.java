d417b46c3292dbd591c0f9711dd6e8fafd235394
saumik <saumikpatel32@gmail.com>
2020-08-04 13:24:56 -0400

    add to cart UI

--- app/src/main/java/com/example/theglam/ProductDetails.java
+++ app/src/main/java/com/example/theglam/ProductDetails.java
@@ -5,21 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class ProductDetails extends AppCompatActivity {
 ImageView back;
+Button addtocart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_details);
         back=findViewById(R.id.back);
+        addtocart=findViewById(R.id.addtocart);
 
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(getApplicationContext(),Home.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        addtocart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),Cart.class);
                 startActivity(i);
                 finish();
             }
